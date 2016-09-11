b4316dc88b6de7a8d8de504b8b843cb7a389d44b
LeonidKuzmin <leonid.kuzmin@mail.ru>
2016-09-11 08:50:37 +0500

    Add files via upload

---
+++ Demo1_Task5.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Простое меню для терминальной программы на C#
+    Использование. Вызываемые из меню методы могут быть в других классах
+    
+    Леонид Кузьмин
+*/
+
+using System;
+
+namespace SimpleMenu_for_ConsoleApplication
+{
+    class Task5
+    {
+        static public void Run()
+        {
+            Console.WriteLine("Task5.Run()");
+            Console.ReadKey();
+        }
+    }
+}
